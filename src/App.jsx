389a30ae5ec9c34d77ee23571364c281aa80be68
Melvin Marcello <melvinmarcello2978@gmail.com>
2023-06-14 16:44:20 +0700

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import NavigationBar from './components/NavigationBar'
 import Main from './components/Main'
 import Skills from './components/Skills'
 import About from './components/About'
-import '../src/App.css'
 import Project from './components/Project'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import '../src/App.css'
 
 
 
